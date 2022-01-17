96e9c8f777e8a917ae3ca63de9c805da915b4cca
EC2 Default User <ec2-user@ip-172-31-4-222.ec2.internal>
2022-01-17 13:55:49 +0000

    update

---
+++ app/controllers/postimages_controller.rb
@@ -0,0 +1,36 @@
+class PostimagesController < ApplicationController
+  def new
+    @postimage = Postimage.new
+  end
+
+  def create
+    @postimage = Postimage.new(postimage_params)
+    @postimage.user_id = current_user.id
+      if @postimage.save
+        redirect_to postimages_path
+      else
+        render :new
+      end
+  end
+
+  def index
+    @postimages = Postimage.page(params[:page])
+  end
+
+  def show
+    @postimage = Postimage.find(params[:id])
+    @post_comment = PostComment.new
+  end
+  
+  def destroy
+    @postimage = Postimage.find(params[:id])
+    @postimage.destroy
+    redirect_to postimages_path
+  end
+  
+  
+  private
+  def postimage_params
+    params.require(:postimage).permit(:shop_name, :image, :caption)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: "homes#top"
+  get "/homes/about"  => "homes#about", as: "about"
+  resources :postimages, only: [:new, :index, :show, :create, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
+end
+
